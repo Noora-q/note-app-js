d882452d965e6d0271153d980d8807b1f1bf631e
Flora Harvey <fharvey555@gmail.com>
2017-03-28 18:34:43 +0100

    Create spec folder

--- noteListModel.js
+++ noteListModel.js
@@ -11,3 +11,16 @@
 
   exports.NoteList = NoteList;
 })(this);
+<<<<<<< HEAD
+=======
+
+NoteList.prototype.createNote = function (text) {
+  return this.notes.push(new Note(text));
+};
+
+NoteList.prototype.displayNotes = function () {
+  this.notes.forEach(function(note){
+    return note.text
+  });
+};
+>>>>>>> 58d8afda028c0f7bc2190e74faef441508ae9a14
