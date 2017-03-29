6e06db0bab67610dcb98efc442c8c8e2deb08a52
Flora Harvey <fharvey555@gmail.com>
2017-03-29 16:02:14 +0100

    Add method to controller to add html to view

--- noteController.js
+++ noteController.js
@@ -10,6 +10,15 @@
     this.noteList.createNote(text);
   };
 
+  NoteController.prototype.createView = function() {
+    return this.view = new NoteListView(this.noteList);
+  };
+
+  NoteController.prototype.addHTML = function() {
+    var element = document.getElementById("app");
+    element.innerHTML = this.view.generateHTML();
+  };
+
   exports.NoteController = NoteController;
 
 })(this);
--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -1,43 +1,81 @@
 "strict mode";
 
-function noteControllerCanBeInstantiated () {
+// (function(exports) {
+//   function Test () {
+//   }
 
-  function NoteListDouble() {};
-  var noteListDouble = new NoteListDouble();
-  var controller = new NoteController(noteListDouble);
+//   exports.Test = {
+//     noteControllerCanBeInstantiated: noteControllerCanBeInstantiated
+//   }
+// })(this);
 
-  assert.isTypeOf(controller, NoteController, "NoteController was not instantiated")
+
+function NoteListDouble() {
+  this.createNoteCallCount = 0;
+};
+
+NoteListDouble.prototype = {
+  createNote: function() {
+    this.createNoteCallCount++;
+  },
+  viewNotes: function() {
+    return ["hello"]
+  }
+};
+
+function DivDouble() {
+  this.innerHtmlCallCount = 0;
+  this.id = "app"
+};
+
+DivDouble.prototype = {
+  innerHtml: function() {
+    this.innerHtmlCallCount++;
+  }
 };
 
 
-
-function noteControllerSavesNotelist () {
-
+function noteControllerCanBeInstantiated () {
   function NoteListDouble() {};
-
   var noteListDouble = new NoteListDouble();
   var controller = new NoteController(noteListDouble);
+  assert.isTypeOf(controller, NoteController, "NoteController was not instantiated")
+};
 
+function noteControllerSavesNotelist () {
+  function NoteListDouble() {};
+  var noteListDouble = new NoteListDouble();
+  var controller = new NoteController(noteListDouble);
   assert.isSameObject(controller.noteList, noteListDouble, "NoteController does not save notelist as property");
 };
 
 
 function noteControllerAddsNoteToNoteList () {
-  function NoteListDouble() {
-    this.createNoteCallCount = 0;
-  };
-  NoteListDouble.prototype = {
-    createNote: function() {
-      this.createNoteCallCount++;
-    }
-  };
-
   var noteListDouble = new NoteListDouble();
   var controller = new NoteController(noteListDouble);
   controller.addNote("Hello")
   assert.isEqual(noteListDouble.createNoteCallCount, 1)
 };
 
+function noteControllerCreatesNoteListView () {
+  var noteListDouble = new NoteListDouble();
+  var controller = new NoteController(noteListDouble);
+  controller.createView();
+  assert.isEqual(controller.view.noteList, noteListDouble)
+};
+
+// function noteControllerInsertsHtml () {
+//   var noteListDouble = new NoteListDouble();
+//   var controller = new NoteController(noteListDouble);
+//   var div = new DivDouble();
+//   controller.createView();
+//   console.log(document.getElementById("app"))
+//   controller.addHTML()
+//   assert.isEqual(div.innerHtmlCallCount, 1)
+// };
+
+
 noteControllerCanBeInstantiated ();
 noteControllerSavesNotelist ();
 noteControllerAddsNoteToNoteList ()
+noteControllerCreatesNoteListView ()
